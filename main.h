27676876fd734af2aed17eca8dac0bb7c3394197
Kali <kali@kali>
2022-10-18 18:50:27 -0400

    Updated first and second functions

--- main.h
+++ main.h
@@ -67,7 +67,7 @@ int print_hexa_upper(va_list types, char buffer[], int flags, int width,
 int print_hexa(va_list types, char map_to[], char buffer[], int flags,
 		char flag_ch, int width, int precision, int size);
 
-/* Function to print non printable characters */
+/* Function to print non printable chars */
 int print_non_printable(va_list types, char buffer[], int flags, int width,
 		int precision, int size);
 
